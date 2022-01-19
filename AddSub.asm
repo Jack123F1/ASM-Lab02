758a4cd4abb99d0a9bdc58f0b3494d6fe3eac033
傑克 楊 <a1573157317005@yahoo.com.tw>
2022-01-19 14:23:50 +0800

    AddSub

--- AddSub.asm
+++ AddSub.asm
@@ -6,18 +6,20 @@ TITLE Add and Subtract, Version 2         (AddSub2.asm)
 INCLUDE Irvine32.inc
 
 .data
-val1     dword  10000h
-val2     dword  40000h
+val1     dword  80000h
+val2     dword  10000h
 val3     dword  20000h
+val4     dword  30000h
 finalVal dword  ?
 
 .code
 main PROC
 
-	mov	eax,val1			; start with 10000h
-	add	eax,val2			; add 40000h
+	mov	eax,val1			; start with 80000h
+	sub	eax,val2			; subtract 10000h
 	sub	eax,val3			; subtract 20000h
-	mov	finalVal,eax		; store the result (30000h)
+	sub	eax,val4			; subtract 30000h
+	mov	finalVal,eax		; store the result (20000h)
 	call	DumpRegs			; display the registers
 
 	exit
